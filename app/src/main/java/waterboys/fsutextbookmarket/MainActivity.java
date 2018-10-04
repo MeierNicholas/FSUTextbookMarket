1cb3bd1b8baa6880283c08231712d526d60f8281
Nicholas Meier <nlm15@my.fsu.edu>
2018-10-04 10:55:36 -0400

    Initial commit

---
+++ app/src/main/java/waterboys/fsutextbookmarket/MainActivity.java
@@ -0,0 +1,21 @@
+package waterboys.fsutextbookmarket;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void yourMethodName(View v){
+        startActivity(new Intent(MainActivity.this, Main2Activity.class));
+    }
+
+}
